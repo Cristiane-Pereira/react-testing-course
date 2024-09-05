94cae5ca15ccb1a3ca06a57550c414af98a684ed
Cristiane <cristiane.pereira901113@gmail.com>
2024-09-05 18:03:29 -0300

    teste função

--- src/utils.test.js
+++ src/utils.test.js
@@ -1,8 +1,19 @@
 import { describe, expect, it } from 'vitest';
+import { range } from './utils';
 
-// utils.test.js
+// Teste 1, verifica se o valor é o mesmo d equivalente testado ...
+// describe('utils', () => {
+//     it('checks that 1 equals 1', () => {
+//         expect(1).toEqual(1);
+//     })
+// })
+
+// Teste 2, testa o retorno e saída de uma função ...
 describe('utils', () => {
-    it('checks that 1 equals 1', () => {
-        expect(1).toEqual(1);
+    describe("range", () => {
+        it("return correct result from 1-6 range", () => {
+            const result = range(1, 6);
+            expect(result).toEqual([1, 2, 3, 4, 5]);
+        });
     })
 })
